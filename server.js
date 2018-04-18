267256b8ee2293f4f28c27153ca793351d2de889
JosephRivera10 <joseph.rivera.09@gmail.com>
2018-04-18 17:27:27 -0500

    deploy

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});
+  app.use(express.static("./client/build"));
+// app.get('*', function (req, res) {
+//   const index = path.join(__dirname, 'build', 'index.html');
+//   res.sendFile(index);
+// });
 }
 // configure using our exported passport function.
 // we need to pass the express app we want configured!
